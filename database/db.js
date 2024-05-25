b71b07f3c37f267d9b8faf09e33cedf04a5d7c14
aravindkk004 <aravind463kk30@gmail.com>
2024-05-25 14:54:53 +0530

    final commit

--- database/db.js
+++ database/db.js
@@ -1,15 +1,16 @@
-import pg from 'pg';
-import env from 'dotenv';
-env.config();
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+dotenv.config(); 
+
+const db = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
 
-db.connect();
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to PostgreSQL successfully');
+});
 
 export default db;
